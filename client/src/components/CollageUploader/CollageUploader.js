1a78cea851613d8d618d1522fd192c587230cb15
Shalom Jerusalem <jerusalemshalom23@gmail.com>
2020-05-16 04:21:56 +0800

    File selected handler process

--- client/src/components/CollageUploader/CollageUploader.js
+++ client/src/components/CollageUploader/CollageUploader.js
@@ -1,13 +1,26 @@
-import React, { useRef } from 'react';
+import React, { useRef, useState } from 'react';
 
-const CollageUploader = ({ position }) => {
+const CollageUploader = ({ position, selectedFileHandler }) => {
   const inputFile = useRef(null);
 
-  const handleUploadedFile = e => {
-    console.log(e.target.files);
-  }
+  const handleOnFileChange = event => {
+    const fileList = event.target.files;
 
-  const handleBrowseClick = e => {
+    if (!fileList.length) {
+      return;
+    }
+
+    const file = fileList[0];
+
+    // Check if the file uploaded is an image
+    if (file && file.type.indexOf('image') === -1) {
+      return;
+    }
+
+    selectedFileHandler(file);
+  };
+
+  const handleBrowseClick = event => {
     inputFile.current.click();
   };
 
@@ -15,7 +28,7 @@ const CollageUploader = ({ position }) => {
     <div className="collage-block-uploader d-flex align-items-center justify-content-center">
       <div className="w-100 text-center px-5">
         <p>Photo {position}</p>
-        <input type="file" ref={inputFile} className="d-none" accept="image/png,image/jpeg" onChange={handleUploadedFile}/>
+        <input type="file" ref={inputFile} className="d-none" accept="image/png,image/jpeg" onChange={handleOnFileChange}/>
         <button className="btn btn-light btn-lg d-block shadow text-uppercase w-100" onClick={handleBrowseClick}>Browse</button>
       </div>
     </div>
