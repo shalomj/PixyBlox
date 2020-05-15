1ec968b94c3cc5b20fdfe8ee9ce99bdcea4fa0f1
Shalom Jerusalem <jerusalemshalom23@gmail.com>
2020-05-16 04:22:35 +0800

    Progress bar component

---
+++ client/src/components/ProgressBar/ProgressBar.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+const ProgressBar = ({ percent, height }) => {
+  const progressHeight = height || 5;
+
+  console.log(progressHeight);
+
+  return (
+    <div className="d-flex align-items-center justify-content-center px-5 w-100 h-100">
+      <div className="progress w-100" style={{height: `${progressHeight}px`}}>
+        <div className="progress-bar" role="progressbar" style={{width: `${percent}%`}} aria-valuenow={percent} aria-valuemin="0" aria-valuemax="100"></div>
+      </div>
+    </div>
+  );
+}
+
+export default ProgressBar;
