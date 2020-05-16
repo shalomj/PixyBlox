87550ae6a287523ca0434204fd3c3152c3ef0ccf
Shalom Jerusalem <jerusalemshalom23@gmail.com>
2020-05-16 15:02:19 +0800

    AJAX upload process

--- client/src/components/CollageHeader/CollageHeader.js
+++ client/src/components/CollageHeader/CollageHeader.js
@@ -1,11 +1,11 @@
 import React from 'react';
 import Layouts from '../Layouts';
 
-const CollageHeader = () => {
+const CollageHeader = ({ saveHandler }) => {
   return (
     <header id="collage-header" className="d-flex align-items-center justify-content-between">
       <Layouts />
-      <button className="btn btn-primary shadow">Save</button>
+      <button className="btn btn-primary shadow" onClick={saveHandler}>Save</button>
     </header>
   )
 };
