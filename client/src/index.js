65e059e1e7595d277a22202c28782485851666d8
Shalom Jerusalem <jerusalemshalom23@gmail.com>
2020-05-16 09:31:56 +0800

    Import cropperjs to the project

--- client/src/index.js
+++ client/src/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './assets/fonts/Montserrat-Regular.ttf';
+import 'cropperjs/dist/cropper.css';
 import './assets/styles/main.scss';
 import App from './components/App';
 
