12a80dda8fe7985573d2ebdffe21ee33b6379b7d
Shalom Jerusalem <jerusalemshalom23@gmail.com>
2020-05-17 04:27:24 +0800

    Refactor ImageCropper component to rerender once the selected layout has changed

--- client/src/components/ImageCropper/ImageCropper.js
+++ client/src/components/ImageCropper/ImageCropper.js
@@ -1,29 +1,65 @@
 import React, { useRef, useEffect } from 'react';
 import Cropper from 'cropperjs';
 
-const ImageCropper = ({ image, setCropDetail }) => {
+const ImageCropper = ({ image, layout, position, setCropDetail }) => {
   const img = useRef(null);
 
+  const getCanvasAndCropSize = (layout, position) => {
+    const options = {
+      width: 500, 
+      height: 500, 
+    };
+
+    if (layout === 1) {
+      return options;
+    }
+
+    if (layout === 2 || (layout === 3 && position === 1)) {
+      options.width = options.width / 2;
+
+      return options;
+    }
+
+    options.width = options.width / 2;
+    options.height = options.height / 2;
+
+    return options;
+  };
+
   const initCropper = (elem) => {
-    const cropper = new Cropper(elem, {
+    const cropperOpts = getCanvasAndCropSize(layout, position);
+
+    new Cropper(elem, {
       viewMode: 0, 
       dragMode: 'move', 
       responsive: true, 
       modal: false, 
       cropBoxMovable: false, 
       cropBoxResizable: false, 
+      ready() {
+        this.cropper.setCanvasData(cropperOpts);
+        this.cropper.setCropBoxData(cropperOpts);
+      },
       crop(event) {
-        console.log(event.detail);
+        const cropDetail = this.cropper.getData(true);
 
-        setCropDetail(event.detail);
-      }
+        console.log(cropDetail);
+
+        setCropDetail(cropDetail);
+      },
     });
   }
 
   useEffect(() => {
-    initCropper(img.current);
-  }, []);
+    const currentImg = img.current;
 
+    initCropper(img.current);
+
+    return () => {
+      currentImg.cropper.destroy();
+    }
+  }, [layout]);
+  
   return (
     <div className="w-100 h-100">
       <img src={image} ref={img} className="d-none" style={{maxWidth: '100%'}}/>
