438f528d6ae20b0c590c4dd9c8ed9b54ad07a2ed
Shalom Jerusalem <jerusalemshalom23@gmail.com>
2020-05-17 04:37:48 +0800

    Remove unused useState

--- client/src/components/CollageUploader/CollageUploader.js
+++ client/src/components/CollageUploader/CollageUploader.js
@@ -1,4 +1,4 @@
-import React, { useRef, useState } from 'react';
+import React, { useRef } from 'react';
 
 const CollageUploader = ({ position, selectedFileHandler }) => {
   const inputFile = useRef(null);
