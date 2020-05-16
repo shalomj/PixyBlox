5608bf58de5b6dbbc59603ffb9a9e28a6cd305eb
Shalom Jerusalem <jerusalemshalom23@gmail.com>
2020-05-17 04:30:21 +0800

    Set collage block height style

--- client/src/utils/index.js
+++ client/src/utils/index.js
@@ -2,11 +2,13 @@ export function getGridAreaByLayout(layout) {
   return (layout > 1) ? `1 / 1 / ${layout} / auto` : '1'; 
 };
 
-export function getBlockStyleByLayoutPosition(layout, position) {
+export function getBlockStyleByLayoutPosition(layout, position, forLayoutSelect) {
   const style = {};
 
   if (position === 1) {
     style.gridArea = getGridAreaByLayout(layout);
+  } else if (!forLayoutSelect) {
+    style.height = '250px';
   }
 
   return style;
